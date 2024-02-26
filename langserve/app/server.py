ff300b09c6f0fe078f03a6c8b788d1664b771aa4
Mark <github@markedmondson.me>
2024-02-26 11:47:52 +0100

    missing image_talk endpoint

--- langserve/app/server.py
+++ langserve/app/server.py
@@ -11,6 +11,7 @@ logging.info(f"langserve app started with port: {int(os.environ.get('PORT', 8080
 from pirate_speak.chain import chain as pirate_speak_chain
 from csv_agent import agent_executor as csv_agent_chain
 from rag_lance import chain as rag_lance_chain
+from image_talk import chain as image_talk_chain
 
 config, filename = load_config('config/cloud_run_urls.json')
 
@@ -38,6 +39,8 @@ add_routes(app, pirate_speak_chain, path="/pirate_speak")
 add_routes(app, csv_agent_chain, path="/csv_agent")
 
 add_routes(app, rag_lance_chain, path="/rag_lance")
+
+add_routes(app, image_talk_chain, path="/image_talk")
 
 if __name__ == "__main__":
     import uvicorn
